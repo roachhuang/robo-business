d0732532de0f47d226097cdbdc49bd8e984b69cd
roachhuang <giraftw2002@gmail.com>
2020-01-02 12:12:06 +0800

    1st

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Wifi 遙控/避障自走車';
+}
